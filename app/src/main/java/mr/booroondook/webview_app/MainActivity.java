796ffbf46618f2feb0b5590fdc2317ca8041769c
Mr. Booroondook <Mr.Booroondook@gmail.com>
2020-04-16 00:36:36 +0400

    Add WebView

--- app/src/main/java/mr/booroondook/webview_app/MainActivity.java
+++ app/src/main/java/mr/booroondook/webview_app/MainActivity.java
@@ -2,13 +2,33 @@ package mr.booroondook.webview_app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
+@SuppressLint("SetJavaScriptEnabled")
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        WebView webView = findViewById(R.id.web_view);
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+
+        webView.setWebViewClient(new WebViewClient() {
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
+                view.loadUrl(request.getUrl().toString());
+                return true;
+            }
+        });
+
+        webView.loadUrl(getResources().getString(R.string.app_url));
     }
 }
